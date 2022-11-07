61307bd16bf31332d09586b6e30cbc8d28fab9dc
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-07 10:45:43 -0300

    Custom Hook - useRandom

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,20 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+//* styles *//
 import "./App.css";
 
-const getRandomNumberFromApi = async (): Promise<number> => {
-  const res = await fetch(
-    "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new"
-  );
-  const numberString = await res.text();
-
-  return Number(numberString);
-};
+//* hooks *//
+import { useRandom } from "./hooks/useRandom";
 
 export const App = () => {
-  const { isError, data, error, refetch, isFetching } = useQuery(
-    ["randomNumber"],
-    () => getRandomNumberFromApi()
-  );
+  const { isFetching, isError, data, error, refetch } = useRandom();
 
   return (
     <div className="App App-header">
