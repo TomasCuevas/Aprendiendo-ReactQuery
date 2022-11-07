339263e412b1d7d0b56d5f5718279c364cb777d9
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-07 10:04:15 -0300

    Mejorar la experiencia de usuario

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 
 const getRandomNumberFromApi = async (): Promise<number> => {
@@ -7,19 +7,42 @@ const getRandomNumberFromApi = async (): Promise<number> => {
   );
   const numberString = await res.text();
 
+  // throw new Error("Auxilio!!!");
   return Number(numberString);
 };
 
 export const App = () => {
   const [number, setNumber] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
+  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
 
   useEffect(() => {
-    getRandomNumberFromApi().then((num) => setNumber(num));
-  }, []);
+    setIsLoading(true);
+    getRandomNumberFromApi()
+      .then((num) => setNumber(num))
+      .catch((error) => setError(error.message));
+  }, [key]);
+
+  useEffect(() => {
+    if (number) setIsLoading(false);
+  }, [number]);
+
+  useEffect(() => {
+    if (error) setIsLoading(false);
+  }, [error]);
 
   return (
     <div className="App App-header">
-      <h2>Numero aleatorio: {number}</h2>
+      {isLoading ? (
+        <h2>Cargando...</h2>
+      ) : !error ? (
+        <h2>Numero aleatorio: {number}</h2>
+      ) : null}
+      {!isLoading && error ? <h3>{error}</h3> : null}
+      <button onClick={forceRefetch} disabled={isLoading}>
+        Nuevo numero
+      </button>
     </div>
   );
 };
