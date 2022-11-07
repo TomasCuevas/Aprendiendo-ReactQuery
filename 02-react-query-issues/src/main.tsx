26258a6e2d0ef273dbf04ecbf384cf1d3736761f
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-07 11:27:54 -0300

    Instalación - React Query

--- 02-react-query-issues/src/main.tsx
+++ 02-react-query-issues/src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router';
-import { router } from './router';
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-//! Remover enable css source maps
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles.css';
+//* router *//
+import { router } from "./router";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>
-)
+//* styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
