4c0a29ba0784f0244e477832b5d8fc14aa0766da
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-08 10:04:32 -0300

    initialData y placeholderData

--- 02-react-query-issues/src/hooks/useLabels.ts
+++ 02-react-query-issues/src/hooks/useLabels.ts
@@ -15,7 +15,26 @@ const getLabels = async () => {
 
 export const useLabels = () => {
   const labelsQuery = useQuery(["labels"], getLabels, {
-    staleTime: 1000 * 60 * 60,
+    // staleTime: 1000 * 60 * 60,
+    placeholderData: [
+      {
+        id: 739777675,
+        node_id: "MDU6TGFiZWw3Mzk3Nzc2NzU=",
+        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API",
+        name: "Component: Component API",
+        color: "d4c5f9",
+        default: false,
+      },
+      {
+        id: 2281766624,
+        node_id: "MDU6TGFiZWwyMjgxNzY2NjI0",
+        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Scheduling%20Profiler",
+        name: "Component: Scheduling Profiler",
+        color: "1dc3d6",
+        default: false,
+      },
+    ],
+    // initialData: [],
   });
 
   return {
