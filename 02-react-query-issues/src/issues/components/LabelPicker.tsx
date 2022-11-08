f2fc813fa1979cfd910c35985ea8d1405b2272bf
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-08 09:12:28 -0300

    Label Issues - Facebook/React

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,14 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+const getLabels = async () => {
+  const res = await fetch("https://api.github.com/repos/facebook/react/labels");
+  const data = await res.json();
+
+  console.log(data);
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery(["labels"], () => getLabels());
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
