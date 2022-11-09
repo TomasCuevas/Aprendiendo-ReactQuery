6b1f1467fe097f59a45b97b13fc7429cdc6be9d4
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-09 09:44:08 -0300

    Autenticación en GitHub

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
-  headers: {},
+  headers: {
+    Authorization:
+      "Bearer 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",
+  },
 });
