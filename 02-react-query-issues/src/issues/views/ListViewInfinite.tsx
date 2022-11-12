ac2a9938e9814cd5693e0db1ed3446a68993135f
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-12 09:57:41 -0300

    Preparación para el infinite scroll

---
+++ 02-react-query-issues/src/issues/views/ListViewInfinite.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+//* components *//
+import { IssueList } from "../components/IssueList";
+import { LabelPicker } from "../components/LabelPicker";
+
+//* hooks *//
+import { useIssues } from "../../hooks";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+
+//* interface *//
+import { State } from "../../interfaces/issue";
+
+export const ListViewInfinite = () => {
+  const [selectedLabel, setSelectedLabel] = useState<string[]>([]);
+  const [state, setState] = useState<State>();
+
+  const { issuesQuery, page, nextPage, previousPage } = useIssues({
+    labels: selectedLabel,
+    state,
+  });
+
+  const onLabelChange = (labelName: string) => {
+    if (selectedLabel.includes(labelName)) {
+      setSelectedLabel((prev) => prev.filter((label) => label !== labelName));
+    } else {
+      setSelectedLabel((prev) => [...prev, labelName]);
+    }
+  };
+
+  return (
+    <div className="row mt-5">
+      <div className="col-8">
+        {issuesQuery.isLoading ? (
+          <LoadingIcon />
+        ) : (
+          <IssueList
+            issues={issuesQuery.data || []}
+            state={state}
+            onStateChange={(newState) => setState(newState)}
+          />
+        )}
+
+        <button className="btn btn-outline-primary mt-2">Load more</button>
+      </div>
+
+      <div className="col-4">
+        <LabelPicker
+          selectedLabels={selectedLabel}
+          onChange={(labelName) => onLabelChange(labelName)}
+        />
+      </div>
+    </div>
+  );
+};
