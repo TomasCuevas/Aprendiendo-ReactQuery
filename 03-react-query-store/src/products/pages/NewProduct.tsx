edefdac8223db8fbce513dcaaa1d6382c192702a
Tomas Cuevas <memo_mister398@hotmail.com>
2023-10-07 12:47:39 -0300

    React Hook Form - Formulario

--- 03-react-query-store/src/products/pages/NewProduct.tsx
+++ 03-react-query-store/src/products/pages/NewProduct.tsx
@@ -1,27 +1,100 @@
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
 //* NEXTUI *//
 import { Button, Image, Input, Textarea } from "@nextui-org/react";
 
+interface FormInputs {
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
 export const NewProduct: React.FC = () => {
+  const { control, handleSubmit } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex-col w-full">
       <h1 className="text-2xl font-bold">Nuevo producto</h1>
 
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex items-center justify-around">
-          <div className="flex-col w-[500px]">
-            <Input className="mt-2" type="text" label="Titulo del producto" />
-            <Input className="mt-2" type="number" label="Precio del producto" />
-            <Input className="mt-2" type="url" label="Url del producto" />
-            <Textarea className="mt-2" label="Descripcion del producto" />
-            <select className="w-full p-3 mt-2 bg-gray-800 rounded-md">
-              <option value="men's clothing">Men's clothing</option>
-              <option value="women's clothing">Women's clothing</option>
-              <option value="jewelery">Jewelery</option>
-              <option value="electronics">Electronics</option>
-            </select>
+          <div className="flex-col w-[500px] gap-2 flex">
+            <Controller
+              control={control}
+              name="title"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  type="text"
+                  value={field.value}
+                  onChange={field.onChange}
+                  label="Titulo del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="price"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  value={String(field.value)}
+                  onChange={field.onChange}
+                  type="number"
+                  label="Precio del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="image"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input
+                  type="url"
+                  value={field.value}
+                  onChange={field.onChange}
+                  label="Url del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="description"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Textarea
+                  value={field.value}
+                  onChange={field.onChange}
+                  label="Descripcion del producto"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="description"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <select
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="w-full p-3 bg-gray-800 rounded-md"
+                >
+                  <option value="men's clothing">Men's clothing</option>
+                  <option value="women's clothing">Women's clothing</option>
+                  <option value="jewelery">Jewelery</option>
+                  <option value="electronics">Electronics</option>
+                </select>
+              )}
+            />
 
-            <br />
-            <Button className="mt-2" color="primary">
+            <Button type="submit" className="w-full" color="primary">
               Crear
             </Button>
           </div>
